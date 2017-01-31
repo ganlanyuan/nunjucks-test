eba3ca0b64b19b935d56a91e768f04ee583466ef
William Lin 22 <ganlanyuan@gmail.com>
2017-01-31 17:22:55 -0500

    add htmltidy, test global varible

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 var config = {
   sassLang: 'libsass',
   sourcemaps: '../sourcemaps',
-  server: {
-    base: '.',
-    hostname: '0.0.0.0',
-    keepalive: true,
-    stdio: 'ignore',
-  },
+  // server: {
+  //   base: '.',
+  //   hostname: '0.0.0.0',
+  //   keepalive: true,
+  //   stdio: 'ignore',
+  // },
   browserSync: {
-    proxy: '0.0.0.0:8000',
-    open: true,
+    server: {
+      baseDir: './'
+    },
+    // proxy: '0.0.0.0:8000',
+    open: false,
     notify: false
   },
 
-  markup: {
+  html: {
     src: 'templates/*.njk',
     watch: 'templates/**/*.njk',
     options: {
       watch: true,
-      noCache: true
+      noCache: true,
     },
     dest: ''
   },
@@ -35,36 +38,73 @@ var gulp = require('gulp');
 var php = require('gulp-connect-php');
 var browserSync = require('browser-sync').create();
 var rename = require("gulp-rename");
-var nunjucks = require('gulp-nunjucks');
+// var nunjucks = require('gulp-nunjucks');
+var nunjucksRender = require('gulp-nunjucks-md');
+var prettify = require('gulp-html-prettify');
+var htmltidy = require('gulp-htmltidy');
+var gulpdata = require('gulp-data');
 
 var data = require('./templates/data.json');
 data.year = new Date().getFullYear();
+// data.imageCount = 1;
 
 function errorlog (error) {  
   console.error.bind(error);  
   this.emit('end');  
 }  
 
-gulp.task('markup', () =>
-  gulp.src(config.markup.src)
-    .pipe(nunjucks.compile(data), config.markup.options)
-    .pipe(rename(function (path) {
-      path.extname = ".html";
+gulp.task('html', function () {
+  return gulp.src(config.html.src)
+    .pipe(gulpdata(data))
+    .pipe(nunjucksRender({
+      path: ['templates/'], // String or Array
+      envOptions: config.html.options,
+      manageEnv: function(environment) {
+        environment.addGlobal('imageCount', 1)
+      }
     }))
-    .pipe(gulp.dest(config.markup.dest))
-);
+    .pipe(htmltidy({
+      doctype: 'html5',
+      wrap: 0,
+      hideComments: true,
+      indent: true,
+      'indent-attributes': false,
+      'drop-empty-elements': false,
+      'force-output': true
+    }))
+    .pipe(gulp.dest(config.html.dest))
+});
+
+// gulp.task('html', () =>
+//   gulp.src(config.html.src)
+//     .pipe(nunjucks.compile(data), config.html.options)
+//     .pipe(rename(function (path) {
+//       path.extname = ".html";
+//     }))
+//     // .pipe(prettify({indent_char: ' ', indent_size: 2}))
+//     .pipe(htmltidy({
+//       doctype: 'html5',
+//       wrap: 0,
+//       hideComments: true,
+//       indent: true,
+//       'indent-attributes': false,
+//       'drop-empty-elements': false,
+//       'force-output': true
+//     }))
+//     .pipe(gulp.dest(config.html.dest))
+// );
 
 // Server
-gulp.task('server', function () {
-  php.server(config.server);
-});
-gulp.task('sync', ['server'], function() {
+// gulp.task('server', function () {
+//   php.server(config.server);
+// });
+gulp.task('sync', function() {
   browserSync.init(config.browserSync);
 });
 
 // watch
 gulp.task('watch', function () {
-  // gulp.watch(config.markup.watch, ['markup']);
+  gulp.watch(config.html.watch, ['html']);
   gulp.watch(config.watch.html).on('change', browserSync.reload);
 })
 
